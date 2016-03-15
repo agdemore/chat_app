3997eae7bc0e8058292d10b6509439a89939bc45
Алексей Ралдугин <alekseyraldugin@Mac-mini-Aleksej.local>
2016-03-15 17:40:53 +0300

    add ui for side dialogs and users/chats names

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const ipcRenderer = require('electron').ipcRenderer;
 var request = require('request');
 let vkAuth = require('./vk_auth');
 let fs = require('fs');
+let Promise = require('promise');
 
 vkAuth.authenticate();
 
@@ -13,83 +14,146 @@ let appData = JSON.parse(fs.readFileSync(__dirname + '/app_data.json'));
 const accessToken = appData.access_token;
 const userId = appData.user_id;
 
+function getFriends() {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://api.vk.com/method/friends.get?user_id=' + userId + '&fields=nickname&v=5.50'
+        }, function(error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                let friendsJson = JSON.parse(response.body);
+                let friends = friendsJson.response.items;
+                resolve(friends);
+            }
+        });
+    })
+}
+
+function createListOfFriends() {
+    getFriends()
+        .then(friends => {
+            let toWrite = [];
+            for (let i = 0; i < friends.length; i++) {
+                let uid = friends[i]['id'];
+                let firstName = friends[i]['first_name'];
+                let lastName = friends[i]['last_name'];
+                let elem = {
+                    'id': uid,
+                    'first_name': firstName,
+                    'last_name': lastName
+                };
+                toWrite.push(elem);
+            }
+            fs.writeFileSync(__dirname + '/friends_data.json', JSON.stringify(toWrite));
+        })
+}
+
+function getDialogs() {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://api.vk.com/method/messages.getDialogs?access_token=' + accessToken + '&v=5.33'
+        }, function(error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                let dialogsJson = JSON.parse(response.body);
+                let dialogs = dialogsJson.response.items;
+                resolve(dialogs);
+            }
+        });
+    })
+}
+
+function createMessage(dialogElement) {
+    // create message element
+    var li = document.createElement('li');
+    li.className = 'message';
+    var div = document.createElement('div');
+    div.className = 'message-inner';
+    var innerUl = document.createElement('ul');
+    innerUl.className = "inner-ul";
+    var photoLi = document.createElement('li');
+    photoLi.className = "photo";
+    var messageLi = document.createElement('li');
+    var dateSpan = document.createElement('span');
+    messageLi.className = 'inner-ul-message';
+    dateSpan.className = 'date';
+    var messageInfoUl = document.createElement('ul');
+    messageInfoUl.className = "message-info";
+    var userInfo = document.createElement('span');
+    userInfo.className = "user-name";
+    var messageInfoLi = document.createElement('li');
+    messageInfoLi.className = 'message-li';
+    var mess = document.createElement('li');
+    mess.className = "mess";
+    var date = dialogElement['message']['date'] * 1000;
+    var normalDate = new Date(date);
+
+    //display last message
+    if (dialogElement['message']['attachments']) {
+        mess.innerHTML = dialogElement['message']['attachments'][0]['type']
+    } else {
+        mess.innerHTML = dialogElement['message']['body'];
+    }
+
+    if (dialogElement['message']['read_state'] == 0) {
+        li.className = 'message message-unread'
+    }
+    if (dialogElement['unread']) {
+        dateSpan.innerHTML = '+' + dialogElement['unread'].toString();
+    } else {
+        dateSpan.innerHTML = normalDate.toLocaleDateString();
+    }
+    photoLi.innerHTML = '[Photo]';
 
 
-//
-// window.onload = function() {
-//     ipcRenderer.send('ready-to-create-dialogs-list');
-// };
-//
-// ipcRenderer.on('create-dialogs-list', function(event, dialogs, at) {
-//     var dialogList = document.getElementById('dialogs');
-//     for (var i = 0; i < dialogs.length; i++) {
-//         var li = document.createElement('li');
-//         li.className = 'message';
-//         var div = document.createElement('div');
-//         div.className = 'message-inner';
-//         var innerUl = document.createElement('ul');
-//         var photoLi = document.createElement('li');
-//         var messageLi = document.createElement('li');
-//         var dateSpan = document.createElement('span');
-//         messageLi.className = 'inner-ul-message';
-//         dateSpan.className = 'date';
-//         var messageInfoUl = document.createElement('ul');
-//         messageInfoUl.className = "message-info";
-//         var userInfo = document.createElement('span');
-//         userInfo.className = "user-name";
-//         var messageInfoLi = document.createElement('li');
-//         messageInfoLi.className = 'message-li';
-//         var mess = document.createElement('li');
-//         mess.className = "mess";
-//         var date = dialogs[i]['message']['date'] * 1000;
-//         var normalDate = new Date(date);
-//
-//         if (dialogs[i]['message']['attachments']) {
-//             mess.innerHTML = dialogs[i]['message']['attachments']['type']
-//         } else {
-//             mess.innerHTML = dialogs[i]['message']['body'];
-//         }
-//         if (dialogs[i]['message']['read_state'] == 0) {
-//             li.className = 'message message-unread'
-//         }
-//         if (dialogs[i]['unread']) {
-//             dateSpan.innerHTML = '+' + dialogs[i]['unread'].toString();
-//         } else {
-//             dateSpan.innerHTML = normalDate.toLocaleDateString();
-//         }
-//         photoLi.innerHTML = '[Photo]';
-//         //userInfo.innerHTML = 'Name Surname';
-//
-//         messageInfoLi.appendChild(userInfo);
-//         messageInfoLi.appendChild(dateSpan);
-//         messageInfoUl.appendChild(messageInfoLi);
-//         messageInfoUl.appendChild(mess);
-//         messageLi.appendChild(messageInfoUl);
-//         innerUl.appendChild(photoLi);
-//         innerUl.appendChild(messageLi);
-//         div.appendChild(innerUl);
-//         li.appendChild(div);
-//         dialogList.appendChild(li);
-//
-//
-//         if (dialogs[i]['message']['user_id']) {
-//             var uid = dialogs[i]['message']['user_id'];
-//             request({
-//                 url: "https://api.vk.com/method/users.get?user_id=" + uid +
-//                     "&fields=contacts&access_token=" + at + "&v=5.8"
-//             }, function(error, response, body) {
-//                 if (error) {
-//                     console.log('ooops! error with user');
-//                 }
-//             var u = userJson.response;
-//                 if (u[0]) {
-//                     userInfo.innerHTML = u[0]['first_name'] + ' ' + u[0]['last_name'];
-//                 }
-//             });
-//         }
-//
-//     }
-//
-//     console.log(dialogs);
-//     console.log(dialogs[4]['message']['body']);
-// });
+    // ------------------ add message name or chat name ------------------------
+    let u = JSON.parse(fs.readFileSync(__dirname + '/friends_data.json'));
+    if (dialogElement['message']['user_id'] && dialogElement['message']['title'] == ' ... ') {
+        let uid = dialogElement['message']['user_id'];
+        for (let i = 0; i < u.length; i++) {
+            if (uid == u[i]['id']) {
+                let messageName = u[i]['first_name']+ ' ' + u[i]['last_name'];
+                userInfo.innerHTML = messageName;
+            }
+        }
+    } else {
+        let chatName = dialogElement['message']['title'];
+        let uid = dialogElement['message']['user_id'];
+        userInfo.innerHTML = chatName;
+        for (let i = 0; i < u.length; i++) {
+            if (uid == u[i]['id']) {
+                let chatLastSender = u[i]['first_name']+ ' ' + u[i]['last_name'];
+            }
+        }
+    }
+    //--------------------------------------------------------------------------
+
+    messageInfoLi.appendChild(userInfo);
+    messageInfoLi.appendChild(dateSpan);
+    messageInfoUl.appendChild(messageInfoLi);
+    messageInfoUl.appendChild(mess);
+    messageLi.appendChild(messageInfoUl);
+    innerUl.appendChild(photoLi);
+    innerUl.appendChild(messageLi);
+    div.appendChild(innerUl);
+    li.appendChild(div);
+    console.log(li);
+    return li;
+}
+
+function createDialogsUi() {
+    getDialogs()
+        .then(dialogs => {
+            console.log(dialogs);
+            let dialogList = document.getElementById('dialogs');
+            let u = JSON.parse(fs.readFileSync(__dirname + '/friends_data.json'));
+            for (let i = 0; i < dialogs.length; i++) {
+                dialogList.appendChild(createMessage(dialogs[i]));
+            }
+        })
+}
+
+createListOfFriends();
+createDialogsUi();
