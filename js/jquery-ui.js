97a9d9f99763e8e06b79d25db532fbd50c0fd4a2
agdemore <agdemore@gmail.com>
2016-03-30 14:30:59 +0300

    minor fixes

--- js/jquery-ui.js
+++ js/jquery-ui.js
@@ -1,31 +1,32 @@
 var $ = require('jquery');
 
-var originWidth = 1200;
+var originWidth = 900;
 var originHeight = 800;
 
 $(document).on('click', "li.message", function() {
     var height = $('div.right-menu').height();
     $('.right-menu').scrollTop(height)
-    if ($(window).width() < 1100) {
+    if ($(window).width() < originWidth) {
         $('.left-menu').addClass('hide-element');
         $('.nav-expand').toggleClass('open');
     }
 
 });
 
+// to display chat window on start
 var appWidth = $(window).width();
 if (appWidth == originWidth) {
-    $('.show-left-menu').hide();
-} else if (appWidth > originWidth) {
-    $('.show-left-menu').hide();
-} else if (appWidth < originWidth) {
     $('.show-left-menu').show();
+} else if (appWidth > originWidth) {
+    $('.show-left-menu').show();
+} else if (appWidth < originWidth) {
+    $('.show-left-menu').hide();
 }
 
 
 $(window).resize(function() {
     var wWidth = $(window).width();
-    if (wWidth < 1100) {
+    if (wWidth < 870) {
         $('.left-top').addClass('hide-element');
 
         $('.left-menu').addClass('hide-element');
@@ -35,7 +36,7 @@ $(window).resize(function() {
         $('.right-menu').addClass('right-menu-min');
 
         $('.show-left-menu').show();
-    } else if (wWidth >= 1200) {
+    } else if (wWidth >= 900) {
         if ($('.left-top').hasClass('hide-element'))
             $('.left-top').removeClass('hide-element');
         if ($('.left-menu').hasClass('hide-element'))
