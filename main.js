4f21ea3f83042afe48d6ebdc922d217389c57b6f
agdemore <agdemore@gmail.com>
2016-04-12 15:18:26 +0300

    add tray logic

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+'use strict';
+
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 const ipcMain = require('electron').ipcMain;
 var fs = require('fs');
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
 var mainWindow = null;
+let appIcon = null;
 
 app.on('window-all-closed', function() {
   	if (process.platform != 'darwin') {
@@ -32,6 +37,22 @@ app.on('ready', function() {
     } catch(e) {
         mainWindow.loadURL('file://' + __dirname + '/templates/login.html');
     }
+
+    appIcon = new Tray(__dirname + '/icon/vk-16-b.png');
+    let contextMenu = Menu.buildFromTemplate([
+        { label: 'Minimize to tray', click: () => { mainWindow.minimize() }},
+        { label: 'Quit', click: () => { app.quit() }}
+    ]);
+    appIcon.setToolTip('vk chat');
+    appIcon.setContextMenu(contextMenu);
+
+    appIcon.on('right-click', function(event, bounds) {
+        if (mainWindow.isVisible()) {
+            mainWindow.minimize();
+        } else {
+            mainWindow.show();
+        }
+    })
 
 
   	mainWindow.webContents.openDevTools();
