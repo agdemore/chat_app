7f7ca65bb52ce05574cad5ddff90de78f80a112f
agdemore <agdemore@gmail.com>
2016-04-14 15:41:36 +0300

    asve for future working React

--- helper.js
+++ helper.js
@@ -7,57 +7,76 @@ let fs = require('fs');
 let Promise = require('promise');
 let jQuery = require('jquery');
 let notifier = require('node-notifier')
-//
-// // vkAuth.authenticate();
-// // console.log(__dirname);
-// // let appData = JSON.parse(fs.readFileSync(__dirname + 'app_data.json'));
-// // console.log(appData);
-//
-// class Helper {
-//     constructor() {
-//         this.accessToken = appData.access_token;
-//         this.userId = appData.user_id;
-//     }
-//
-//     getFriends() {
-//         return new Promise((resolve, reject) => {
-//             request({
-//                 url: 'https://api.vk.com/method/friends.get?user_id=' + this.userId + '&fields=nickname,photo_50&v=5.50'
-//             }, function(error, response, body) {
-//                 if (error) {
-//                     reject(error);
-//                 } else {
-//                     let friendsJson = JSON.parse(response.body);
-//                     let friends = friendsJson.response.items;
-//                     resolve(friends);
-//                 }
-//             });
-//         })
-//     }
-//
-//     createListOfFriends() {
-//         this.getFriends()
-//             .then(friends => {
-//                 let toWrite = {};
-//                 for (let i = 0; i < friends.length; i++) {
-//                     let uid = friends[i]['id'];
-//                     let firstName = friends[i]['first_name'];
-//                     let lastName = friends[i]['last_name'];
-//                     let photoUrl = friends[i]['photo_50']
-//                     // let elem = {};
-//                     toWrite[uid] = {
-//                             'first_name': firstName,
-//                             'last_name': lastName,
-//                             'photo': photoUrl
-//                     };
-//                     // toWrite.push(elem);
-//                 }
-//                 fs.writeFileSync(__dirname + '/friends_data.json', JSON.stringify(toWrite));
-//             })
-//     }
-//
-// }
-//
+
+vkAuth.authenticate();
+let appData = JSON.parse(fs.readFileSync(__dirname + '/app_data.json'));
+
+class Helper {
+    constructor() {
+        this.accessToken = appData.access_token;
+        this.userId = appData.user_id;
+    }
+
+    getFriends(userId) {
+        return new Promise((resolve, reject) => {
+            request({
+                url: 'https://api.vk.com/method/friends.get?user_id=' + userId + '&fields=nickname,photo_50&v=5.50'
+            }, function(error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    let friendsJson = JSON.parse(response.body);
+                    let friends = friendsJson.response.items;
+                    resolve(friends);
+                }
+            });
+        })
+    }
+
+    createListOfFriends() {
+        this.getFriends(this.userId)
+            .then(friends => {
+                let toWrite = {};
+                for (let i = 0; i < friends.length; i++) {
+                    let uid = friends[i]['id'];
+                    let firstName = friends[i]['first_name'];
+                    let lastName = friends[i]['last_name'];
+                    let photoUrl = friends[i]['photo_50']
+                    // let elem = {};
+                    toWrite[uid] = {
+                            'first_name': firstName,
+                            'last_name': lastName,
+                            'photo': photoUrl
+                    };
+                    // toWrite.push(elem);
+                }
+                fs.writeFileSync(__dirname + '/friends_data.json', JSON.stringify(toWrite));
+            })
+    }
+
+    getDialogs(offset) {
+        return new Promise((resolve, reject) => {
+            request({
+                url: 'https://api.vk.com/method/messages.getDialogs?access_token=' + this.accessToken + '&offset='+ offset +'&v=5.33'
+            }, function(error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    let dialogsJson = JSON.parse(response.body);
+                    let dialogs = dialogsJson.response.items;
+                    resolve(dialogs);
+                }
+            });
+        })
+    }
+
+
+
+
+}
+
 // let apiHelper = new Helper();
-//
-// module.exports.apiHelper = apiHelper;
+
+// apiHelper.getDialogs('0').then(dialogs => {console.log(dialogs);})
+
+export default Helper;
--- js/entry.js
+++ js/entry.js
@@ -6,16 +6,70 @@ var ReactDom = require('react-dom');
 
 var request = require('request');
 // let vkAuth = require('../vk_auth');
-let apiHelper = require('../helper');
+// let Helper = require('../helper');
+import Helper from '../helper';
 let fs = require('fs');
 let Promise = require('promise');
 let jQuery = require('jquery');
 let notifier = require('node-notifier')
-//
-// vkAuth.authenticate();
-// console.log(__dirname);
-// let appData = JSON.parse(fs.readFileSync(__dirname + 'app_data.json'));
-// console.log(appData);
+
+let apiHelper = new Helper();
+
+let DialogList = React.createClass({
+    getInitialState: function() {
+        return {items: []}
+    },
+    componentDidMount: function() {
+        apiHelper.getDialogs('0')
+            .then(dialogs => {
+                this.setState({items: dialogs});
+            })
+    },
+    render: function() {
+        console.log(this.state.items);
+        return <DialogListElement items={this.state.items} />
+    }
+});
+
+let DialogListElement = React.createClass({
+    render: function() {
+        let createItem = function(item) {
+            if ({item.message.chat_id}) {
+                let mdate = {item.message.user_id},
+                    mbody = {item.message.body},
+                    cid = {item.message.chat_id},
+                    mtitle = {item.message.title},
+                    cphoto = {item.message.photo_50}
+            } else {
+
+            }
+            let uid = {item.message.user_id},
+                mdate = {item.message.user_id},
+                mbody = {item.message.body},
+                mtitle = {item.message.title},
+            let uphoto = 'lll'
+            return (<li key={item.message.id} className="message">
+                        <div className="message-inner">
+                            {item.message.body}
+                        </div>
+                    </li>);
+        };
+        return <ul className="dialogs-inner">{this.props.items.map(createItem)}</ul>;
+    }
+});
+
+let MessageList = React.createClass({
+    render: function() {
+
+    }
+});
+
+let MessageListElement = React.createClass({
+    render: function() {
+
+    }
+});
+
 
 var App = React.createClass({
   render: function() {
@@ -23,4 +77,4 @@ var App = React.createClass({
   }
 });
 
-ReactDom.render(<App/>, document.getElementById('react-root'));
+ReactDom.render(<DialogList/>, document.getElementById('dialogs'));
